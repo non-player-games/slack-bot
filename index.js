66200a233c19ca13d793995b550f45f5a69b6194
Eric Liao <rcliao01@gmail.com>
2016-04-11 23:27:18 -0700

    feat: add ping reaction to bot

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+'use strict';
+
 const fs = require('fs');
 const Bot = require('slackbots');
-
-'use strict';
+const ping = require('./reactions/ping');
 
 const apiKey = process.env.SLACK_API_KEY;
 const settings = {
   token: apiKey,
   name: 'lopez'
 };
+const params = {
+  icon_emoji: ':cat:'
+};
 
 const bot = new Bot(settings);
 
+bot.on('close', function() {
+  console.log('Bot connection closed');
+});
+
+bot.on('open', function() {
+  console.log('Bot connection started');
+
+  bot.on('message', function(msg) {
+    if (msg.text === 'ping') {
+      bot.postMessage(msg.channel, ping(), params);
+    }
+  });
+});
+
 bot.on('start', function() {
   console.log('Bot started');
-  bot.postMessageToChannel('bot-sandbox', 'Buenos dias. Mi llamo Lopez -- eric');
+
+  bot.postMessageToChannel('bot-sandbox', 'Buenos dias. Mi llamo Lopez');
 });
